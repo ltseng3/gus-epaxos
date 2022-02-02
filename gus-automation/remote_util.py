bc3a037f7bfd54a7c99c9048db665b28d980d6a3
Neo Zhou <zhouaea@bc.edu>
2022-02-02 16:48:03 -0500

    fix multiplex rsync command

--- gus-automation/remote_util.py
+++ gus-automation/remote_util.py
@@ -2,11 +2,10 @@ import subprocess
 
 
 def copy_path_to_remote_host(local_path, remote_url, remote_path, exclude_paths=[]):
-    print('%s:%s' % (remote_url, remote_path))
-    args = ["rsync", "-r", "-e", "'ssh", "-o", "StrictHostKeyChecking=no",  # can connect with machines that are not in the known host list
+    args = ["rsync", "-r", "-e", "ssh", "-o", "StrictHostKeyChecking=no",  # can connect with machines that are not in the known host list
             "-o", "ControlMaster=auto",  # multiplex ssh connections with a single tcp connection
             "-o", "ControlPersist=2m",  # after the first connection is closed, use tcp connection for up to 2 minutes
-            "-o", "ControlPath=~/.ssh/cm-%r@%h:%p'",  # location of the control socket
+            "-o", "ControlPath=~/.ssh/cm-%r@%h:%p",  # location of the control socket
             local_path, "%s:%s" % (remote_url, remote_path)]
     if exclude_paths is not None:
         for i in range(len(exclude_paths)):
