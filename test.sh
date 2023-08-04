1d2b1a0e446d616cf17d8ba5558100c5423984da
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-08-03 23:17:33 -0400

    updated test

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-timeout 180s bin/client -maddr=10.10.1.1 -writes=$1 -c=$2 -T=$3
-python3.8 client_metrics.py 
+timeout 180s bin/client -maddr=10.10.1.1 -writes=0.5 -c=0 -T=4
+python3 client_metrics.py
