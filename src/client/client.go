d7f2158cd2c78a60fee3ba93f82cb94848d9f126
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-08-07 14:25:39 -0400

    debugging

--- src/client/client.go
+++ src/client/client.go
@@ -227,7 +227,6 @@ func simulatedClientReader(reader *bufio.Reader, orInfo *outstandingRequestInfo,
 			break
 		}
 		after := time.Now()
-		log.Println(reply.Value)
 		orInfo.sema.Release(1)
 
 		orInfo.Lock()
