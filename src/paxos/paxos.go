a39e20a13c3a596d11945b1e59eb4ad2f3e5e753
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-08-07 11:23:57 -0400

    debugging

--- src/paxos/paxos.go
+++ src/paxos/paxos.go
@@ -419,6 +419,9 @@ func (r *Replica) handlePropose(propose *genericsmr.Propose) {
 	if propose.Command.Op == state.GET {
 		log.Println("got read")
 		r.readProposal[propose.CommandId] = propose
+		preply := &genericsmrproto.ProposeReplyTS{TRUE, propose.CommandId, 1, 0}
+		r.ReplyProposeTS(preply, propose.Reply)
+		return
 		r.bcastRead(propose.CommandId)
 	} else {
 		for r.instanceSpace[r.crtInstance] != nil {
