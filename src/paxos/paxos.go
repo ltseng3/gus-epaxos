8e5c01c9a335ca1aed677e4e8dfd70ae05c6ee9e
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-08-28 18:31:23 -0400

    logs for debugging

--- src/paxos/paxos.go
+++ src/paxos/paxos.go
@@ -420,6 +420,7 @@ func (r *Replica) bcastCommit(instance int32, ballot int32, command []state.Comm
 }
 
 func (r *Replica) handlePropose(propose *genericsmr.Propose) {
+	log.Println("got proposal: ", propose.Command.Op)
 	// got read command
 	if propose.Command.Op == state.GET {
 		r.readProposal[propose.CommandId] = propose
