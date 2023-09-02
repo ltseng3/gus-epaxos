ddb43ed43adf968fa673412d2f3207685fa3c770
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-02 18:15:01 -0500

    debug sleep

--- src/client/client.go
+++ src/client/client.go
@@ -195,6 +195,7 @@ func simulatedClientWriter(writer *bufio.Writer, lWriter *bufio.Writer, orInfo *
 		}
 		orInfo.startTimes[args.CommandId] = before
 		orInfo.Unlock()
+		time.Sleep(1 * time.Second)
 	}
 }
 
