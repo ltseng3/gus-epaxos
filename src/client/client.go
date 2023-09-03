d7144ce68362cc11bc71beb289db0cc342adbd2a
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-02 19:33:22 -0500

    debugging

--- src/client/client.go
+++ src/client/client.go
@@ -220,7 +220,7 @@ func simulatedClientReader(reader *bufio.Reader, orInfo *outstandingRequestInfo,
 		rtt := (after.Sub(before)).Seconds() * 1000
 		//commitToExec := float64(reply.Timestamp) / 1e6
 		commitLatency := float64(0) //rtt - commitToExec
-
+		log.Println("received: ", reply.CommandId)
 		readings <- &response{
 			after,
 			rtt,
