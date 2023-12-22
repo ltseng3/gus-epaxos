715673a7ae29588de6193a29f06266be94519e58
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-12-22 14:21:01 -0500

    updated compilation

--- compile.sh
+++ compile.sh
@@ -3,4 +3,5 @@ export GOBIN=/root/go/src/gus-epaxos/bin
 go install gus-epaxos/src/master
 go install gus-epaxos/src/server
 go install gus-epaxos/src/client
+go install gus-epaxos/src/epaxosclient
 export GOBIN=
